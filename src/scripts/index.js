1e7845add2320ee798bd4748ab719ed55cd4461d
gmskhiladze <g.mskhiladze99@gmail.com>
2024-07-19 23:34:53 +0400

    [WIP]: navigation submenu logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
-console.log("====================================");
-console.log("works");
-console.log("====================================");
+// header sum menu
+document.addEventListener("DOMContentLoaded", function () {
+  const trigger = document.querySelector(".header_navigation_dropdown");
+  const dropdown = document.querySelector(".header_dropdown_list");
+  const bg = document.querySelector(".header_navigation_dropdown_bg");
+
+  trigger.addEventListener("click", function () {
+    trigger.classList.toggle("active");
+    dropdown.classList.toggle("active");
+    bg.classList.toggle("active");
+  });
+});
+s;
