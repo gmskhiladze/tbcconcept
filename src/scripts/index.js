45fbaae54393f7a0bbb0bef68e6bf6e5146b2a50
gmskhiladze <g.mskhiladze99@gmail.com>
2024-07-20 16:47:21 +0400

    [DONE]: Responsive header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -42,10 +42,11 @@ document.addEventListener("DOMContentLoaded", function () {
   // burger menu
   const burgerTrigger = document.querySelector(".burger_menu_btn");
   const burgerMenu = document.querySelector(".header_menu_mobile");
+  const mainContent = document.querySelector("main");
 
   burgerTrigger.addEventListener("click", function () {
     burgerTrigger.classList.toggle("active");
     burgerMenu.classList.toggle("active");
-    document.body.classList.toggle("hidden");
+    mainContent.classList.toggle("hidden");
   });
 });
